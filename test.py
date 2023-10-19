cf653507b5ab99770f2079745f4ca9b0ad9b549c
khamis_dossary <khamis.dossary@gmail.com>
2023-10-19 14:54:02 +0300

    deleted a test file

--- test.py
+++
@@ -1,9 +0,0 @@
-participants = {
-        'Alice': ['W1'],
-        'Bob': ['W1', 'W2'],
-        'Mohammed': ['W4', 'W1', 'W2', 'W3'],
-        'Ahmed': ['W4', 'W1'],
-        'Ali': ['W3']
-    }
-participants['Mohammed'].remove("W3")
-print(participants)
